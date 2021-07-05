57f4399f1b35de97bf967f69375de9fcf3704fe9
lucas6636 <lucsa6636@gmail.com>
2021-07-05 09:16:30 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+$(function(){
+  $('a[href^="#"]').click(function(){
+    let speed = 500;
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
 
 $('.js-modal-open').on('click', function(){
   var target = $(this).data('target');
